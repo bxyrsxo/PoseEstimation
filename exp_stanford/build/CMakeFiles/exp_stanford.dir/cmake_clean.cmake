80bf88ca50aebaed68fb62cfd39a0a5d111fd776
bxyrsxo <bxyrsxo@gmail.com>
2017-02-09 13:58:00 +0800

    Original Upload

---
+++ exp_stanford/build/CMakeFiles/exp_stanford.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/exp_stanford.dir/exp_stanford.cpp.o"
+  "exp_stanford.pdb"
+  "exp_stanford"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/exp_stanford.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
